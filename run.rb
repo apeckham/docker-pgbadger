935fe9093c55f0c3c4ee2c2b024d3730e87c6454
apeckham <apeckham@gmail.com>
2015-11-16 17:55:54 -0800

    use shelljoin & system(Array) for cleaner ruby system calls

--- run.rb
+++ run.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 require 'aws-sdk'
+require 'shellwords'
 
 ENV['DB_INSTANCE_IDENTIFIER'] or raise 'DB_INSTANCE_IDENTIFIER is required'
 ENV['BUCKET'] or raise 'BUCKET is required'
@@ -11,14 +12,22 @@ files = files.to_h[:describe_db_log_files]
 
 log_file_count = ENV['LOG_FILE_COUNT'] ? ENV['LOG_FILE_COUNT'].to_i : 5
 
-files.last(log_file_count).each do |file|
-  puts file[:log_file_name]
-  system "rds-download-db-logfile --db-instance-identifier #{ENV['DB_INSTANCE_IDENTIFIER']} " +
-             "-I #{ENV['AWS_ACCESS_KEY_ID']} -S #{ENV['AWS_SECRET_ACCESS_KEY']} " +
-             "--log-file-name #{file[:log_file_name]} | pv -s #{file[:size]} >#{File.basename(file[:log_file_name])}"
+downloaded_files = files.last(log_file_count).map do |file|
+  File.basename(file[:log_file_name]).tap do |basename|
+    command = ["rds-download-db-logfile",
+               "--db-instance-identifier", ENV['DB_INSTANCE_IDENTIFIER'],
+               "-I", ENV['AWS_ACCESS_KEY_ID'],
+               "-S", ENV['AWS_SECRET_ACCESS_KEY'],
+               "--log-file-name", file[:log_file_name]]
+
+    puts file[:log_file_name]
+    system "#{Shellwords.shelljoin command} | pv -s #{file[:size]} >#{basename}"
+  end
 end
 
-system("pgbadger -p '%t:%r:%u@%d:[%p]:' *") or raise 'pgbadger failed'
+p downloaded_files
+
+system(*["pgbadger", "-p", "%t:%r:%u@%d:[%p]:"] + downloaded_files) or raise 'pgbadger failed'
 
 s3 = Aws::S3::Resource.new
 obj = s3.bucket(ENV['BUCKET']).object("pgbadger-#{Date.today}.html")
